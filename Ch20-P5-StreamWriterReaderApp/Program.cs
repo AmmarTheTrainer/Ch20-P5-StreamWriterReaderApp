319dfa923be14183590afd9c3f46541c5992d251
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-12-13 12:00:48 +0500

    Project Created

---
+++ Ch20-P5-StreamWriterReaderApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch20_P5_StreamWriterReaderApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
